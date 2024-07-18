229aba59c36a5ac39fb7556e5477d193a050d763
Nathan <gallon.nathan@gmail.com>
2024-07-18 22:21:44 +0200

    fixed landscape bug roatation

--- src/lib/components/DeviceMockup.svelte
+++ src/lib/components/DeviceMockup.svelte
@@ -22,61 +22,44 @@
     }
   </script>
   
-  <div class={`device ${deviceProps.device} ${color} ${landscape ? 'landscape' : ''}`} style="width: {deviceProps.width}px; height: {deviceProps.height}px;">
-    
-      {#if src}
-        <iframe src={src} class="screen" frameborder="0"></iframe>
-      {:else}
-        <slot></slot>
-      {/if}
-
+  <div class={`device-container`}>
+    <div class={`device ${deviceProps.device} ${color} ${landscape ? 'landscape' : ''}`} style="width: {deviceProps.width}px; height: {deviceProps.height}px;">
+      <div class="screen-container">
+        {#if src}
+          <iframe src={src} class="screen" frameborder="0"></iframe>
+        {:else}
+          <slot></slot>
+        {/if}
+      </div>
+    </div>
   </div>
   
   <style>
-   .device {
-    position: relative;
-    margin: 20px;
-    overflow: hidden;
-    background: #fff;
-    border: 1px solid #ccc;
-  }
-  
-  .device .screen-container {
-    position: absolute;
-    overflow: hidden;
-  }
-  
-  .device .screen {
-    border: none;
-    width: 100%;
-    height: 100%;
-  }
-  
-  .device.landscape {
-    transform: rotate(90deg);
-    transform-origin: center;
-  }
-  
-  .iphone-x {
-    border-radius: 36px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    border: 10px solid darkgray;
-  }
-  
-  .iphone8 {
-    border-radius: 20px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  }
-  
-  .ipad {
-    border-radius: 24px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  }
-  
-  .macbook {
-    border-radius: 12px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  }
-  
+    .device-container {
+      display: inline-block;
+      position: relative;
+    }
+    .device-container.landscape {
+      transform: rotate(90deg);
+      transform-origin: center;
+    }
+    .device {
+      position: relative;
+      margin: 20px;
+      overflow: hidden;
+      background: #fff;
+      border: 10px solid darkgray;
+    }
+    .device .screen-container {
+      position: absolute;
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+    }
+    .device .screen {
+      border: none;
+      width: 100%;
+      height: 100%;
+    }
   </style>
   
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,11 @@
 <script>
-	import DeviceMockup from '$lib/components/DeviceMockup.svelte';
-</script>
+	import DeviceMockup from "$lib/components/DeviceMockup.svelte";
 
+	
+  </script>
+  
+  <DeviceMockup device="iPad" landscape={false} color="gold" src="http://localhost:18080/convertigo/projects/QuestHunter/DisplayObjects/mobile/index.html">
+  </DeviceMockup>
+  
 
-	<DeviceMockup device="MacBook Pro" color="gold" src="http://localhost:18080/convertigo/projects/QuestHunter/DisplayObjects/mobile/index.html">
-	</DeviceMockup>
-
-
-
-
-<style>
-	.flex-center-col {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		height: 100vh;
-	}
-</style>
+  
